72ce2d32ff3c90a617b82663309217d5022aef66
Tim Hodson <tim@timhodson.com>
2026-03-10 15:05:39 +0000

    faff with styling

--- src/app/talis-aspire-module/customComponentMappings.ts
+++ src/app/talis-aspire-module/customComponentMappings.ts
@@ -2,6 +2,6 @@ import { TarlBookmarkButtonComponent } from './tarl-bookmark-button/tarl-bookmar
 import { TarlRelatedListsComponent } from './tarl-related-lists/tarl-related-lists.component';
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
-  ['nde-record-availability-after', TarlBookmarkButtonComponent],
-  ['nde-full-display-service-container-after', TarlRelatedListsComponent]
+  ['nde-record-availability-bottom', TarlBookmarkButtonComponent],
+  ['nde-full-display-service-container-after', TarlRelatedListsComponent],
 ]);
